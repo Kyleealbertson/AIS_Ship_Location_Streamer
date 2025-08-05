5a4edb3d8233084af3b5f80871353a47b20019a2
Kyleealbertson <78563551+Kyleealbertson@users.noreply.github.com>
2025-08-05 07:50:18 -0400

    added in MessageTypeChoice param

--- Source/connect_ais_stream.py
+++ Source/connect_ais_stream.py
@@ -3,13 +3,13 @@ import websockets
 import json
 from datetime import datetime, timezone
 
-async def connect_ais_stream():
+async def connect_ais_stream(MessageTypeChoice: str="PositionReport"):
 
     async with websockets.connect("wss://stream.aisstream.io/v0/stream") as websocket:
-        subscribe_message = {"APIKey": "a07afc829e58c75ca32938ee0951ed37c5bf8509",  # Required !
-                             "BoundingBoxes": [[[-92.951985,41.278138],[-75.681477,49.482688]]], # Required!
+        subscribe_message = {"APIKey": "a07afc829e58c75ca32938ee0951ed37c5bf8509",
+                             "BoundingBoxes": [[[-93, 41], [-75, 50]]],  # Required !
                              #"FiltersShipMMSI": ["368207620", "367719770", "211476060"], # Optional!
-                             "FilterMessageTypes": ["PositionReport"]} 
+                             "FilterMessageTypes": [MessageTypeChoice]} 
 
         subscribe_message_json = json.dumps(subscribe_message)
         await websocket.send(subscribe_message_json)
@@ -17,14 +17,15 @@ async def connect_ais_stream():
         async for message_json in websocket:
             message = json.loads(message_json)
 
-            if "MessageType" not in message:
+            '''if "MessageType" not in message:
                 print("Non-data message:", message)
-                continue
+                continue'''
             message_type = message["MessageType"]
 
-            if message_type == "PositionReport":
+            if message_type == MessageTypeChoice:
+                print(message)
                 # the message parameter contains a key of the message type which contains the message itself
-                ais_message = message['Message']['PositionReport']
+                ais_message = message['Message'][MessageTypeChoice]
                 print(f"[{datetime.now(timezone.utc)}] ShipId: {ais_message['UserID']} Latitude: {ais_message['Latitude']} Latitude: {ais_message['Longitude']}")
 
 if __name__ == "__main__":
